d812c475d16aa8816846e92c27e92f99509833b8
amusabayana <anotidamusabayana@gmail.com>
2023-02-23 20:00:13 +0200

    debug

---
+++ 03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests the function to see if it is posiive or negative
+ * Return - 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+
+return (0);
+}
+
