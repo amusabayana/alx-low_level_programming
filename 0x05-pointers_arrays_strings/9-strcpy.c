c03ace99dab82cf56fe23754dd4e3889b0ffb5ef
Anotida Musabayana <anotidamusabayana@gmail.com>
2023-02-28 13:51:31 -0800

    tasl 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* char *_strcpy - function that copies the string pointed to by src
+* @dest: copying to
+* @src: copying from
+* Return: string
+*/
+char *_strcpy(char *dest, char *src)
+{
+int a = 0;
+int b = 0;
+while (*(src + a) != '\0')
+{
+a++;
+}
+for ( ; b < a ; b++)
+{
+dest[b] = src[b];
+}
+dest[a] = '\0';
+return (dest);
+}
