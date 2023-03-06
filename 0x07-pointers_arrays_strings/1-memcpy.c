b63718649c16cda8454648bb93e8a85a86a37071
Anotida Musabayana <anotidamusabayana@gmail.com>
2023-03-06 02:16:27 -0800

    tasks

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_memcpy - a function that copies memory to a new location
+ *@dest: where memory is stored
+ *@src: where memory is copied to
+ *@n: number of bytes
+ *
+ *Return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int x = 0;
+	int y = n;
+
+	for (; x < y; x++)
+	{
+		dest[x] = src[x];
+		n--;
+	}
+	return (dest);
+}
+
