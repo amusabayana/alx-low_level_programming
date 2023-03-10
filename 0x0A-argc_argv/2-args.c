d79715ade1ac2013ed4f55837d646729a967db88
amusabayana <anotidamusabayana@gmail.com>
2023-03-11 01:15:13 +0200

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /*
- * main - prints all arguments that is recieves
+ * main- prints all arguments the function  receives
+ * main: prnts arguments that are received
  * @argc: stores the number of arguments
  * @argv: stores the array of arguments made
  * Return: always return 0 for success
