7586cbb8b32cab066cc5f253748dc644b524d094
amusabayana <anotidamusabayana@gmail.com>
2023-03-10 17:15:07 +0200

    commits

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input 1
+ * @c: input 2
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+
+	for (; s[x] >= '\0'; x++)
+	{
+		if (s[x] == c)
+			return (&s[x]);
+	}
+	return (0);
+}
