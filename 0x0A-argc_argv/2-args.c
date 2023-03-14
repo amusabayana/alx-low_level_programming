eee2ce49af4fa7e8b9eeeee22c3debe98e884226
Anotida Musabayana <anotidamusabayana@gmail.com>
2023-03-14 10:16:38 -0700

    folders

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,13 +3,14 @@
 
 /*
  * main- prints all arguments the function  receives
- * main: prnts arguments that are received
+ * main - prnts arguments that are received
  * @argc: stores the number of arguments
  * @argv: stores the array of arguments made
  * Return: always return 0 for success
  */
 
 int main(int argc, char *argv[])
+/*main -prints all arguments that are recieved*/
 {
 int x;
 for (x = 0; x < argc; x++)
