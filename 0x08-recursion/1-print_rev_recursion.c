0fd9e27e6a033ad0ffc16692c8785c2fed13935b
amusabayana <anotidamusabayana@gmail.com>
2023-06-09 00:00:15 +0200

    updates

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s - string to be printted
+ */
+
+void _print_rev_recursion(char *s)
+{
+  
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial
+ * @n: input factorial number
+ * Return: the factorial result of' n'
+ */
+
+
+int factorial(int n)
+{
+if (n == 0)
+return (1);
+if (n < 0)
+return (-1)
+return (n * factorial(n - 1))
+}
