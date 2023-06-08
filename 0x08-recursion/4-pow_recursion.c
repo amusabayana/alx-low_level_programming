eb8cdcd9e24945ca2875e1d553f1ad279a778eb2
amusabayana <anotidamusabayana@gmail.com>
2023-06-09 00:21:46 +0200

    updates

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,2 +1,14 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - sum  of x raised to the power of y
+ * @x: initial value to raise
+ * @y: power of
+ * Return: result of the maths
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+return (x * _pow_recursion(x, y - 1));
+}
